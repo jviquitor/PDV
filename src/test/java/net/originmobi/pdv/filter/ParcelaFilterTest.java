3c36427ec0c7eca69a378fb6ae10e71fa1ddf3be
jviquitor <60361075+jviquitor@users.noreply.github.com>
2024-05-06 18:58:01 -0300

    Create ParcelaFilterTest.java

---
+++ src/test/java/net/originmobi/pdv/filter/ParcelaFilterTest.java
@@ -0,0 +1,26 @@
+package net.originmobi.pdv.filter;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.Test;
+
+public class ParcelaFilterTeste{
+	
+	private ParcelaFilter testeParcela;
+	
+	@BeforeAll
+	public void inicializa() {
+		testeParcela = new ParcelaFilter();
+		testeParcela.setNome("Geladeira");
+	}
+	
+	
+	@Test
+	public void getNomeTest() {
+		inicializa();
+		String nome = testeParcela.getNome();
+		Assertions.assertEquals("Geladeira", nome);	
+	}
+	
+}
